e75d137256c66727050867c0cd19378db95466f7
Andrew Dimarogonas <adimarog@asu.edu>
2019-10-09 19:55:14 -0700

    Initial Commit

---
+++ AddingMachine.java
@@ -0,0 +1,45 @@
+package cse360assign2;
+
+public class AddingMachine {
+	private int total;
+	
+	/**
+	 * initializes initial variables
+	 */
+	public AddingMachine() {
+		total=0;
+	}
+	/**
+	 * Returns total
+	 * @return
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	/**
+	 * adds to total
+	 * @param value
+	 */
+	public void add(int value) {
+		
+	}
+	/**
+	 * subtracts from total
+	 * @param value
+	 */
+	public void subtract(int value) {
+		
+	}
+	/**
+	 * returns entire history of operations
+	 */
+	public String toString() {
+		return "";
+	}
+	/**
+	 * clears entire history of operations and the total
+	 */
+	public void clear() {
+		
+	}
+}
